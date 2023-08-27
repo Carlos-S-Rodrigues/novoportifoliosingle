d03785f76c0bc9a9cc87f76ee86673f2b947a6f3
Carlos-S-Rodrigues <carlos.schuenck.rodrigues@gmail.com>
2023-08-27 20:00:11 -0300

    feat: hamburguer menu and style

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AboutMe from "./components/AboutMe"
+import HeaderMenu from "./components/HeaderMenu"
+
+function App() {
+  
+
+  return (
+    <>
+      <HeaderMenu/>
+      <AboutMe/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,14 @@
+
+
+function AboutMe() {
+  return (
+    <div id="aboutme" className="flex flex-col mt-16 ml-4 mp-4">
+      <h1 className="text-xl text-sky-600 font-bold pb-6">Frontend Developer & Graphic Design </h1>
+
+      <p>As a developer i try to apply all my knowledge of coding to get to the best and clean result.</p>
+      <p>Use all my skills as a designer to provide a single and unique experience to the user. </p>
+    </div>
+  )
+}
+
+export default AboutMe
